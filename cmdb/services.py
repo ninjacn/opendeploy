33fb0aea913295900d78d69578b7c22e4adad245
Pengming Yao <x@ninjacn.com>
2018-12-16 20:41:31 +0800

    aliyun

--- cmdb/services.py
+++ cmdb/services.py
@@ -5,25 +5,34 @@ from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException 
 from tencentcloud.cvm.v20170312 import cvm_client, models 
 
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
+from aliyunsdkecs.request.v20140526 import StopInstanceRequest
+
 from setting.services import SettingService
 
 
 class Qcloud():
-    
-    def get_allhost(self, region="ap-beijing"):
-        try: 
-            settingService = SettingService()
-            auth_info = settingService.get_public_cloud_info()
-            if auth_info is None:
-                return None
 
-            cred = credential.Credential(auth_info.qcloud_secret_id, auth_info.qcloud_secret_key) 
+    def __init__(self, key, secret):
+        self.key = key
+        self.secret = secret
+    
+    def get_all_region(self):
+        pass
+
+    def get_allhost(self, region="ap-beijing"):
+        self.region = region
+        try: 
+            cred = credential.Credential(self.key, self.secret) 
             httpProfile = HttpProfile()
             httpProfile.endpoint = "cvm.tencentcloudapi.com"
 
             clientProfile = ClientProfile()
             clientProfile.httpProfile = httpProfile
-            client = cvm_client.CvmClient(cred, region, clientProfile) 
+            client = cvm_client.CvmClient(cred, self.region, clientProfile) 
 
             req = models.DescribeHostsRequest()
             params = '{}'
@@ -34,3 +43,28 @@ class Qcloud():
 
         except TencentCloudSDKException as err: 
             print(err) 
+
+class Aliyun():
+
+    def __init__(self, key, secret):
+        self.key = key
+        self.secret = secret
+
+    def get_all_region(self):
+        pass
+    
+    def get_allhost(self, region="cn-beijing"):
+        self.region = region
+        try: 
+            client = AcsClient(
+                    self.key,
+                    self.secret,
+                    self.region,
+            )
+            request = DescribeInstancesRequest.DescribeInstancesRequest()
+            request.set_PageSize(10)
+
+            response = client.do_action_with_exception(request)
+            print(response)
+        except: 
+            pass
