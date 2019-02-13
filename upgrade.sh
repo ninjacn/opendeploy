1aa4f122a2d48fe69cfc65fea7a72a011321d067
Pengming Yao <x@ninjacn.com>
2019-02-13 17:55:27 +0800

    upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # 升级服务
 
-#git clean -df &&
-#    git reset --hard HEAD &&
+git clean -df &&
+    git reset --hard HEAD &&
     git pull origin master &&
+    docker-compose down;
+    docker-compose -f docker-compose.yml -f docker-compose-prod.yml up -d && 
     docker-compose exec web python manage.py migrate --noinput
+
+if [ "$?" == 0 ]
+then
+    docker-compose ps
+    printf '\E[32m'; echo "升级完成"; printf '\E[0m'
+else
+    printf '\E[31m'; echo "升级失败"; printf '\E[0m'
+    exit 1
+fi
